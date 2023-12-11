cbcf27eddf8c125d1a917758eb103ac4dec05363
Giovanni Torelli <giovanni.walter@outlook.com>
2023-12-11 11:48:58 -0300

    Finished Exercise 1

--- Arrays/src/Exercicios/Exercicio1.java
+++ Arrays/src/Exercicios/Exercicio1.java
@@ -1,4 +1,44 @@
 package Exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] num = new int[10];
+        int numero, posicao = -1;
+        boolean encontrado = false;
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Digite o " + (i + 1) + "ª número: ");
+            num[i] = input.nextInt();
+
+            for(int j = 0; j < num.length; j++) {
+                if(j != i) {
+                    while (num[i] == num[j]) {
+                        System.out.println("Os números não podem se repetir! Digite um novo número");
+                        num[i] = input.nextInt();
+                    }
+                }
+            }
+        }
+        System.out.println("Digite o número que você deseja encontrar: ");
+        numero = input.nextInt();
+
+        for(int i = 0; i < 10; i++) {
+            if(num[i] == numero) {
+                encontrado = true;
+                posicao = i;
+            }
+        }
+
+        if(encontrado) {
+            System.out.println("O número " + numero + " está na posição: " + posicao);
+        } else {
+            System.out.println("O número " + numero + " não foi encontrado!");
+        }
+
+    }
+
 }
